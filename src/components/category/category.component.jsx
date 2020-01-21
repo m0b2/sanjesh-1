df57895856c8c49b14a708e897879f44e852affd
M0BiN <veryhighsigh@gmail.com>
2020-01-22 03:19:20 +0330

    change category style

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -9,47 +9,41 @@ export default ({ title, icon, current, total, index }) => {
 
 
     return (
-        <Slide right={(index%2===0)} left={(index%2!==0)}>
-        <div className='category'>
-            <div className='category-up-div'>
-                <img className='category-icon' src={icon} />
-                <p className='category-title' >{title}</p>
-            </div>
-            <div className='category-down-div'>
-                <div className='category-static-text'>
-                    <p className='category-not-answered'>{total + '/'}</p>
-                    <p className='category-answered'>
-                        <CountUp
-                            end={current}
-                            start={0}
-                            duration={2.2}
+        <Slide right={(index % 2 === 0)} left={(index % 2 !== 0)}>
 
-                        />
-                    </p>
-                </div>
+            <div className='category'>
+                    <img className='category-icon' src={icon} />
+                    <p className='category-title' >{title}</p>
 
-                <Progress className='category-progress' percent={current} status="success"
-                    style={{
-                        direction: 'rtl',
 
-                    }}
-                    theme={{
-                        success: {
-                            color: '#59C8F3'
-                        },
-                        active: {
-                            symbol: '😀',
-                            color: '#fbc630'
-                        },
-                        default: {
-                            symbol: '😱',
-                            color: '#fbc630'
-                        }
-                    }}
-                />
             </div>
 
-        </div>
+
+
+            {/* <div className='category'>
+                <div className='category-up-div'>
+                    <img className='category-icon' src={icon} />
+                    <p className='category-title' >{title}</p>
+
+
+                </div> */}
+            {/* <div className='category-down-div'>
+                    <div className='category-static-text'>
+                        <p className='category-not-answered'>{total + '/'}</p>
+                        <p className='category-answered'>
+                            <CountUp
+                                end={current}
+                                start={0}
+                                duration={2.2}
+
+                            />
+                        </p>
+                    </div>
+
+                </div> */}
+
+            {/* </div> */}
+
         </Slide>
     )
 
