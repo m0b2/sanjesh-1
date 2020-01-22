b8d7dfc42c9df10e958d69f54252bfcdd02b7905
M0BiN <veryhighsigh@gmail.com>
2020-01-23 02:37:12 +0330

    Updates

---
+++ precache-manifest.c0e14532fc961181ce3509ee5b30b310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2258a75d3c9abe63ad20f4d88d677ae",
+    "url": "/sanjesh/index.html"
+  },
+  {
+    "revision": "2a8b84c4eae23fb4332f",
+    "url": "/sanjesh/static/css/2.533d620f.chunk.css"
+  },
+  {
+    "revision": "7399c30d0378939adb62",
+    "url": "/sanjesh/static/css/main.92038205.chunk.css"
+  },
+  {
+    "revision": "2a8b84c4eae23fb4332f",
+    "url": "/sanjesh/static/js/2.71575efc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sanjesh/static/js/2.71575efc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7399c30d0378939adb62",
+    "url": "/sanjesh/static/js/main.e921a5ef.chunk.js"
+  },
+  {
+    "revision": "ce5ac4c5b2ac497a0a84",
+    "url": "/sanjesh/static/js/runtime-main.687c3594.js"
+  }
+]);
