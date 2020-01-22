1ef973c7ea72f2e05a08a1bf69f8190e31fb5d66
M0BiN <veryhighsigh@gmail.com>
2020-01-22 16:37:31 +0330

    add some reducer such as qt

---
+++ src/redux/question.type-reducer/question.type.jsx
@@ -0,0 +1,15 @@
+const Question_Type_Reducer = (state = 0, action) => {
+
+    switch (action.type) {
+        case 'SET_QUESTION_TYPE': return {
+            currentUser: action.payload
+        }
+        default: return state;
+    }
+
+
+
+};
+
+
+export default Question_Type_Reducer;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import UserReducer from './user-reducer/user-reducer';
+import Question_Type from './question.type-reducer/question.type';
 
 
 export default combineReducers({
-    user:UserReducer
+    user:UserReducer,
+    question_type:Question_Type
 })
