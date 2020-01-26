69970efb069c0161da79b3274f1dc68de300d10e
m-mgh404 <m.mgh.uni95@gmail.com>
2020-01-26 17:14:36 +0330

    test commit gogosh

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Home from './pages/home/home.page';
-import Categories from './pages/categories/categories.page';
-import Footer from './components/footer/footer.component';
-import Question_review from './pages/question-review/question-review.page';
-import Notification from './pages/notification/notification.page';
-import './App.css';
-import { Route, Switch,Redirect } from "react-router-dom";
+import React from "react";
+import Home from "./pages/home/home.page";
+import Categories from "./pages/categories/categories.page";
+import Footer from "./components/footer/footer.component";
+import Question_review from "./pages/question-review/question-review.page";
+import Notification from "./pages/notification/notification.page";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-
+// man amade am vay vay
 
 function App() {
-
   return (
-    <div className='app'>
+    <div className="app">
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/question" component={Categories} />
@@ -20,11 +19,9 @@ function App() {
         <Route exact path="/notif" component={Notification} />
         <Route exact path="/search" component={Notification} />
         <Redirect to="/home" />
-
       </Switch>
-      {(window.screen.width < 421) ? <Footer /> : null}
+      {window.screen.width < 421 ? <Footer /> : null}
     </div>
-
   );
 }
 
