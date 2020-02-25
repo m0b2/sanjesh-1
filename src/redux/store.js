1f87f4bcb093e773ffdcf2ea7718d7bdb5cfc2aa
M0BiN <veryhighsigh@gmail.com>
2020-02-25 16:48:51 +0330

    REMOVE MIDDLEWARES

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,8 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middleWares = [logger];
+// const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+const store = createStore(rootReducer);
 
 export default store;
